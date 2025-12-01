e838e778601b9db3656925d493edd2868d8ae432
Winipedia <win.steveker@gmx.de>
2025-12-01 23:16:45 +0000

    test adedd

--- tests/test_winiutils/test_dev/test_tests/test_fixtures/test_fixture.py
+++ tests/test_winiutils/test_dev/test_tests/test_fixtures/test_fixture.py
@@ -1,1 +1,8 @@
 """module."""
+
+from collections.abc import Callable
+
+
+def test_keyring_cleanup(keyring_cleanup: Callable[[str, str], None]) -> None:
+    """Test function."""
+    assert callable(keyring_cleanup), "Expected keyring_cleanup to be callable"
