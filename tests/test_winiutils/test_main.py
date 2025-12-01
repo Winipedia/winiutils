fd8b6bff5b832743a9c3c02d0cc3ebf59658b006
Winipedia <win.steveker@gmx.de>
2025-12-01 14:04:16 +0000

    keyrings alt added as dev

--- tests/test_winiutils/test_main.py
+++ tests/test_winiutils/test_main.py
@@ -7,7 +7,7 @@ from pyrig.src.os.os import run_subprocess
 def test_main() -> None:
     """Test func for main."""
     project_name = PyprojectConfigFile.get_project_name()
-    stdout = run_subprocess(["poetry", "run", project_name, "--help"]).stdout.decode(
+    stdout = run_subprocess(["uv", "run", project_name, "--help"]).stdout.decode(
         "utf-8"
     )
     assert project_name in stdout
