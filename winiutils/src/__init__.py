98dd39ca61116469edd5f839fa5f1abb34ccf601
Winipedia <winipedia@gmx.de>
2025-12-07 00:52:15 +0000

    fix

--- winiutils/src/__init__.py
+++ winiutils/src/__init__.py
@@ -1,10 +1,1 @@
-"""src package.
-
-This package contains the core utility modules for winiutils:
-
-Modules:
-    data: Data processing utilities including DataFrame cleaning and structures.
-    iterating: Iteration utilities and concurrent processing tools.
-    oop: Object-oriented programming patterns including metaclasses and mixins.
-    security: Cryptography and secure storage utilities.
-"""
+"""src package."""
