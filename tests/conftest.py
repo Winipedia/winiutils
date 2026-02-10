be3876a6ba23c02412ab3e2a1617fd9cf06e991b
Winipedia <winipedia@gmx.de>
2026-02-10 15:21:24 +0000

    pyrig update

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,3 @@ infrastructure, including fixtures, hooks, and test utilities.
 """
 
 pytest_plugins = ["pyrig.rig.tests.conftest"]
-
-"""Pytest configuration for tests.
-
-This module configures pytest plugins for the test suite, setting up the necessary
-fixtures and hooks for the different
-test scopes (function, class, module, package, session).
-It also import custom plugins from tests/base/scopes.
-This file should not be modified manually.
-"""
-
-pytest_plugins = ["pyrig.dev.tests.conftest"]
