002a2e8c411365a95b1f3b6cdbaaec9c7f38c235
Winipedia <winipedia@gmx.de>
2026-02-12 23:23:16 +0000

    pyri update

--- tests/test_winiutils/test_rig/test_tools/test_pyrigger.py
+++ tests/test_winiutils/test_rig/test_tools/test_pyrigger.py
@@ -6,7 +6,7 @@ from winiutils.rig.tools.pyrigger import Pyrigger
 class TestPyrigger:
     """Test class."""
 
-    def test_get_dev_dependencies(self) -> None:
+    def test_dev_dependencies(self) -> None:
         """Test method."""
         result = Pyrigger.dev_dependencies()
         assert "types-tqdm" in result
