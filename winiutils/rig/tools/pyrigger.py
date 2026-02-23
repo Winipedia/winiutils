d35f3935f228d1090405de3c87d4621966611a68
Winipedia <winipedia@gmx.de>
2026-02-23 23:07:19 +0000

    pyrig update

--- winiutils/rig/tools/pyrigger.py
+++ winiutils/rig/tools/pyrigger.py
@@ -6,6 +6,6 @@ from pyrig.rig.tools.pyrigger import Pyrigger as BasePyrigger
 class Pyrigger(BasePyrigger):
     """Override pyrig's Pyrigger to add custom dev dependencies."""
 
-    def dev_dependencies(self) -> list[str]:
+    def dev_dependencies(self) -> tuple[str, ...]:
         """Add custom dev dependencies to pyrig's default list."""
-        return [*super().dev_dependencies(), "types-tqdm", "types-defusedxml"]
+        return (*super().dev_dependencies(), "types-tqdm", "types-defusedxml")
