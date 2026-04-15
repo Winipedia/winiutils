fcbd96c4c097a432d260d9cdda9a3c35ad855033
Winipedia <winipedia@gmx.de>
2026-04-15 16:14:28 +0100

    pyrig update

---
+++ tests/test_zero.py
@@ -0,0 +1,9 @@
+"""Contains an empty test.
+
+This test exists so that when no tests are written yet pytest runs successfully
+and does not fail because no tests are found and to execute the autouse fixtures.
+"""
+
+
+def test_zero() -> None:
+    """Empty test."""
